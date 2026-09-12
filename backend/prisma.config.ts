6dad8af64b54d19863f0d1b14c017a220a68bafe
Dev Sahani <devrsahani23@gmail.com>
2026-09-12 15:19:41 +0530

    fixing prisma.config.ts

--- backend/prisma.config.ts
+++ backend/prisma.config.ts
@@ -3,13 +3,10 @@ import 'dotenv/config';
 import { defineConfig } from 'prisma/config';
 
 export default defineConfig({
+  earlyAccess: true,
   schema: './prisma/schema.prisma',
-  datasource: {
-    url: process.env.DATABASE_URL,
-  },
   migrations: {
     path: 'prisma/migrations',
-    seed: 'tsx prisma/seed.ts',
   },
 });
 
